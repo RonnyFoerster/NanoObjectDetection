8a6d6d34db38c428bcbc4f31e76a8993c36c7ed8
RonnyFoerster <47888159+RonnyFoerster@users.noreply.github.com>
2019-03-07 18:02:21 +0100

    Monas Daten

--- conf.py
+++ conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('C:/ProgramData/Anaconda3/Lib/site-packages/NanoObjectDetection'))
 
 
 # -- Project information -----------------------------------------------------
