1dda767453e006c24f9efb328df4d5d0c95d1322
RonnyFoerster <47888159+RonnyFoerster@users.noreply.github.com>
2019-03-08 17:37:21 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,9 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('C:/ProgramData/Anaconda3/Lib/site-packages/NanoObjectDetection'))
-sys.path.insert(0, os.path.abspath('C:/ProgramData/Anaconda3/Lib/site-packages/NanoObjectDetection/tutorial'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../NanoObjectDetection'))
 
 
 # -- Project information -----------------------------------------------------
