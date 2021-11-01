85aadd6c23162ac9031327d4d3e90c38529f805f
RonnyFoerster <47888159+RonnyFoerster@users.noreply.github.com>
2021-11-01 15:12:58 +0100

    Comment PostProcessing

--- NanoObjectDetection/PostProcessing.py
+++ NanoObjectDetection/PostProcessing.py
@@ -6,9 +6,9 @@ Created on Tue Jun  1 14:53:27 2021
 """
 
 import numpy as np # Library for array-manipulation
-import pandas as pd # Library for DataFrame Handling
 import matplotlib.pyplot as plt # Libraries for plotting
 import NanoObjectDetection as nd
+
 
 def ForceUltraUniformParticle(sizes_df_lin, ShowPlot = False):
     """
@@ -16,91 +16,97 @@ def ForceUltraUniformParticle(sizes_df_lin, ShowPlot = False):
     """
     nd.logger.info("Drop particles that do not fit into a single ultra uniform particle hypothesis.")
     
-    
+    # select data that is required
     data = sizes_df_lin[["particle", "diameter", "diffusion", "valid frames", "rawmass_max"]]
     data = data.set_index("particle")
+    
+    # sort the data after valid frames - so sort them by precision!
     data = data.sort_values(["valid frames"])
     
-    # data = data[data.diameter < 100]
-    
+    # rolling parameter
     roll_area = 100
+    
+    # size of scatterer in plot
     my_s = 7
     
-    # def MyStd(data, mean):
-        # return np.mean((data-mean)**2)
-    
+    # define standard deviation - this is used for a rolling average later
     MyStd = lambda x: np.sqrt(np.mean((x - np.median(x))**2))
+
+    CreatePlot = True
     
-    # def MyStd(*args):
-    #     return np.mean((x - np.median(x))**2)
-    
-    ii = 0
-    
+    # max z-score
     z_max = 2.576
     # 1 in 1000 events false positive: 1/(1-scipy.special.erf(3.2905/np.sqrt(2)))
-    # 1 in 100 events false positive: 1/(1-scipy.special.erf(2.576/np.sqrt(2)))
     
-    Done = False
+    # Boolean that says if the remaining particles fullfill the Ultra Uniform Particle Hypothesis 
+    UniFormDiameter = False
 
     
     if ShowPlot == True:
+        # create 2x2 subplot if wanted
         fig, ax = plt.subplots(2,2, sharex = True, sharey=True)
     
     # true particle have a DIAMETER within a Gaussian distribution
-    while Done == False:
+    while UniFormDiameter == False:
         nd.logger.debug("length: %.0f", len(data))
-        mean = data["diameter"].median()
         
-        data.loc[:,"std"] = data.diameter.rolling(roll_area, min_periods = 1, center = True).apply(MyStd)
-
+        # estimate the mean as the MEDIAN (remove outliers) of the Uniform Particle distribution
+        mean = data["diameter"].median()
         
         nd.logger.debug("mean: %.0f", mean)
         
+        # estimated std of the data at each specific traj length
+        data.loc[:,"std"] = data.diameter.rolling(roll_area, min_periods = 1, center = True).apply(MyStd)
+
+        # get the z-score of each particles diameter
         data.loc[:,"z"] = np.abs((data["diameter"] - mean) / data["std"])
         
-        if ii == 0:
-            if ShowPlot == True:
-                min_z = 0
-                max_z = data["z"].max()
-                im00 = ax[0,0].scatter(data["valid frames"], data.diameter, s = my_s, c = data["z"], vmin = min_z, vmax = max_z)
-                cbar00 = fig.colorbar(im00, ax = ax[0,0])
-                cbar00.set_label("z-score")
-                
-                min_b = 0
-                max_b = data["rawmass_max"].max()
-                im01 = ax[0,1].scatter(data["valid frames"], data.diameter, s = my_s, c = data["rawmass_max"], vmin = min_b, vmax = max_b, cmap = 'jet')
-                cbar01 = fig.colorbar(im01, ax = ax[0,1])
-                cbar01.set_label("Max Brightness")
+        if (CreatePlot == True) & (ShowPlot == True):
+            # plot things at the beginning
+            min_z = 0
+            max_z = data["z"].max()
+            im00 = ax[0,0].scatter(data["valid frames"], data.diameter, s = my_s, c = data["z"], vmin = min_z, vmax = max_z)
+            cbar00 = fig.colorbar(im00, ax = ax[0,0])
+            cbar00.set_label("z-score")
+            
+            min_b = 0
+            max_b = data["rawmass_max"].max()
+            im01 = ax[0,1].scatter(data["valid frames"], data.diameter, s = my_s, c = data["rawmass_max"], vmin = min_b, vmax = max_b, cmap = 'jet')
+            cbar01 = fig.colorbar(im01, ax = ax[0,1])
+            cbar01.set_label("Max Brightness")
+            
+            CreatePlot = False
         
         if np.max(data["z"]) < z_max:
-            Done = True    
-        
-        data = data[data["z"]<z_max]
-        
-        ii = ii + 1
+            # if all z-score are below the threshold the uniform particle hypothesis is true
+            UniFormDiameter = True    
+            
+        else:
+            # remove all particles beyond the maximum z-score
+            data = data[data["z"] < z_max]
+
     
     
     # true particle have a DIFFUSION within a Gaussian distribution
-    Done = False
+    UniFormDiffusion = False
     
-    while Done == False:
+    # do the same thing for the diffusion now!
+    while UniFormDiffusion == False:
         nd.logger.debug("length: %.0f", len(data))
         mean = data["diffusion"].median()
-        
-        data.loc[:,"std"] = data.diffusion.rolling(roll_area, min_periods = 1, center = True).apply(MyStd)
-        
         nd.logger.debug("mean: %.0f", mean)
         
+        data.loc[:,"std"] = data.diffusion.rolling(roll_area, min_periods = 1, center = True).apply(MyStd)
+          
         data.loc[:,"z"] = np.abs((data["diffusion"] - mean) / data["std"])
         
-        
         if np.max(data["z"]) < z_max:
-            Done = True    
+            # if all z-score are below the threshold the uniform particle hypothesis is true
+            UniFormDiffusion = True    
         
-        data = data[data["z"]<z_max]
-        
-        ii = ii + 1    
-    
+        else:
+            data = data[data["z"] < z_max]
+
     
     
     # give back initial variable of good particles
